ea98e4e0c4cf04ef3002a2b49d98521d34b32a9f
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-06 13:26:19 -0600

    Test Enigma#encrypt with key and date

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,4 +7,14 @@ class EnigmaTest < Minitest::Test
     enigma = Enigma.new
     assert_instance_of Enigma, enigma
   end
+
+  def test_it_can_encrypt_message_w_key_and_date
+    enigma = Enigma.new
+    encryption = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      }
+    assert_equal encryption, enigma.encrypt("hello world", "02715", "040895")
+  end
 end
