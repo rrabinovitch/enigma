e4b5b5b74035aa6f21922eb9f427cf86b116e8da
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-06 14:01:04 -0600

    Outline later required tests

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,11 +10,42 @@ class EnigmaTest < Minitest::Test
 
   def test_it_can_encrypt_message_w_key_and_date
     enigma = Enigma.new
-    encryption = {
+    encryption_result = {
       encryption: "keder ohulw",
       key: "02715",
       date: "040895"
       }
-    assert_equal encryption, enigma.encrypt("hello world", "02715", "040895")
+    assert_equal encryption_result, enigma.encrypt("hello world", "02715", "040895")
   end
 end
+
+
+# ### LATER TESTS
+# def test_it_can_decrypt_message_w_key_and_date
+#   enigma = Enigma.new
+#   decryption_result = {
+#     decryption: "hello world",
+#     key: "02715",
+#     date: "040895"
+#     }
+#   assert_equal decryption_result, enigma.decrypt("keder ohulw", "02715", "040895")
+# end
+#
+#
+# def test_it_can_encrypt_message_w_key_and_no_date
+#   # encrypt a message with a key (uses today's date)
+#   pry(main)> encrypted = enigma.encrypt("hello world", "02715")
+#   #=> # encryption hash here
+# end
+#
+# def test_it_can_decrypt_message_w_key_and_no_date
+#   #decrypt a message with a key (uses today's date)
+#   pry(main) > enigma.decrypt(encrypted[:encryption], "02715")
+#   #=> # decryption hash here
+# end
+#
+# def test_it_can_encrypt_message_w_no_key_and_no_date
+#   # encrypt a message (generates random key and uses today's date)
+#   pry(main)> enigma.encrypt("hello world")
+#   #=> # encryption hash here
+# end
