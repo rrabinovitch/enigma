b003aa5d16e6c99c57d77089fda13efa7eb81d21
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-07 15:16:36 -0600

    Test Key::number

--- test/key_test.rb
+++ test/key_test.rb
@@ -7,4 +7,12 @@ class EnigmaTest < Minitest::Test
     key = Key.new
     assert_instance_of Key, key
   end
+
+  def test_it_can_generate_number
+    number = Key.number
+    assert_instance_of String, number
+    assert_equal 5, number.length
+    assert_equal true, number.to_i.between?(0, 99999)
+    # consider stubbing #rand method for additional testing
+  end
 end
