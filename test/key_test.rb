6c0704a483b23b2505675e39c2f05a763fc21612
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-07 15:21:24 -0600

    Test Key::key_hash

--- test/key_test.rb
+++ test/key_test.rb
@@ -3,16 +3,22 @@ require 'minitest/pride'
 require './lib/key'
 
 class EnigmaTest < Minitest::Test
+  def setup
+    @key = Key.new
+    @number = Key.number
+  end
   def test_it_exits
-    key = Key.new
-    assert_instance_of Key, key
+    assert_instance_of Key, @key
   end
 
   def test_it_can_generate_number
-    number = Key.number
-    assert_instance_of String, number
-    assert_equal 5, number.length
-    assert_equal true, number.to_i.between?(0, 99999)
+    assert_instance_of String, @number
+    assert_equal 5, @number.length
+    assert_equal true, @number.to_i.between?(0, 99999)
     # consider stubbing #rand method for additional testing
+  end
+
+  def test_it_can_create_key_hash
+    assert_instance_of Hash, Key.key_hash
   end
 end
