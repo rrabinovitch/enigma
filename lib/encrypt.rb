a09e065ec8bafb0cbd8537060bbcac19c65fa175
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-08 11:08:11 -0600

    Compose cmd line output

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -17,3 +17,5 @@ end
 writer = File.open(ARGV[1], "w")
 writer.write(encrypted_message)
 writer.close
+
+puts "Created '#{ARGV[1]}' with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
