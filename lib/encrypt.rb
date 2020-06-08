dcb07e3d2af692416e4e9666612804a9a0cca257
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-08 11:06:39 -0600

    Set up conditional for how 'encrypted_message' is defined

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -5,3 +5,11 @@ enigma = Enigma.new
 handle = File.open(ARGV[0], "r")
 message = handle.read
 handle.close
+
+if !ARGV[3].nil? && !ARGV[4].nil?
+  encrypted_message = enigma.encrypt(incoming_text, ARGV[3], ARGV[4])
+elsif !ARGV[3].nil? && ARGV[4].nil?
+  encrypted_message = enigma.encrypt(incoming_text, ARGV[3])
+else
+  encrypted_message = enigma.encrypt(incoming_text)
+end
