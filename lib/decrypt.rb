9fe9d44734827891a946494fba416ea9547f6ae5
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-08 03:04:16 -0600

    Create runner files

---
+++ lib/decrypt.rb
@@ -0,0 +1,10 @@
+# Additionally, you should create a Runner file called
+# decrypt.rb that takes four command line arguments. The
+# first is an existing file that contains an encrypted
+# message. The second is a file where your program should
+# write the decrypted message. The third is the key to be
+# used for decryption. The fourth is the date to be used
+# for decryption. In addition to writing the decrypted
+#   message to the file, your program should output to
+#   the screen the file it wrote to, the key used for
+#   decryption, and the date used for decryption.
