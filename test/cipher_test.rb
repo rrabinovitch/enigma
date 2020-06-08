b1b1e562966da5eda1c723786ab17e72d8a361a3
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-08 02:15:01 -0600

    Move @alphabet attr from Enigma to Cipher

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -16,4 +16,9 @@ class CipherTest < Minitest::Test
     assert_instance_of Date, @cipher.today
   end
 
+  def test_it_has_alphabet_incl_space
+    assert_equal true, @cipher.alphabet.include?(" ")
+    assert_equal 27, @cipher.alphabet.count
+  end
+
 end
