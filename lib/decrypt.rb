a3b8c53cbda0d460644a313609b9690e715132a3
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-08 10:06:11 -0600

    Move runner files into root directory

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -5,6 +5,6 @@
 # write the decrypted message. The third is the key to be
 # used for decryption. The fourth is the date to be used
 # for decryption. In addition to writing the decrypted
-#   message to the file, your program should output to
-#   the screen the file it wrote to, the key used for
-#   decryption, and the date used for decryption.
+# message to the file, your program should output to
+# the screen the file it wrote to, the key used for
+# decryption, and the date used for decryption.
