4b568cef67974976f015fbabec9f46ab7dde4fe7
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-06-08 02:11:02 -0600

    Test Cipher#today

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -4,8 +4,16 @@ require 'minitest/pride'
 require './lib/cipher'
 
 class CipherTest < Minitest::Test
-  def test_it_exists
-    cipher = Cipher.new
-    assert_instance_of Cipher, cipher
+  def setup
+    @cipher = Cipher.new
   end
+
+  def test_it_exists
+    assert_instance_of Cipher, @cipher
+  end
+
+  def test_it_has_todays_date
+    assert_instance_of Date, @cipher.today
+  end
+
 end
